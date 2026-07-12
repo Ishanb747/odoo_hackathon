afb295e8e03efaa0c23385c9a443f39293b3a5ad
Ishan Bhattacharya <ishan@Ishans-MacBook-Air.local>
2026-07-12 16:42:36 +0530

    fix: resolve merge conflicts and add recharts

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -8,16 +8,9 @@ import OrgSetupPage from './pages/OrgSetupPage'
 import BookingsPage from './pages/BookingsPage'
 import MaintenancePage from './pages/MaintenancePage'
 import AuditPage from './pages/AuditPage'
-import {
-  ReportsPage,
-  NotificationsPage,
-} from './pages/PlaceholderPage'
 import AssetsPage from './pages/AssetsPage'
 import AllocationPage from './pages/AllocationPage'
 import DashboardPage from './pages/DashboardPage'
-import { DashboardPage } from './pages/PlaceholderPage'
-import AssetsPage from './pages/AssetsPage'
-import AllocationPage from './pages/AllocationPage'
 import ReportsPage from './pages/ReportsPage'
 import NotificationsPage from './pages/NotificationsPage'
 
